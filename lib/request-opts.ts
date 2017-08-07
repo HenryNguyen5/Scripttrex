1042fabcca5b3fc25e211befa0dbf982e409157f
Mike Stupich <mike.stupich@gmail.com>
2017-08-07 17:47:33 -0400

    Added sigt wallet interaction

--- lib/request-opts.ts
+++ lib/request-opts.ts
@@ -1,5 +1,5 @@
 export default {
 	baseUrl: 'https://bittrex.com/api/v1.1',
-	apikey: 'a8aed82198784f5e9847ef205c17b4b3',
-	apisecret: '1ebe734e88e54e91866959ea9f5e18a8'
+	apikey: 'key',
+	apisecret: 'secret'
 }
