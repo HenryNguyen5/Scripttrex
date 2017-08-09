ec1b047c14e9b7818df8deee8d44804b402a8319
Erik Burton <eburton@ciena.com>
2017-08-09 13:38:15 -0400

    this is definitely everything

--- lib/configs/options.ts
+++ lib/configs/options.ts
@@ -1,10 +1,4 @@
 export default {
   refreshInterval: 1,
-  phoneNumbers: ["+1234567890"],
-  twilio: {
-    accountSid: "",
-    authToken: "",
-    number: ""
-  },
-  coin: "DOGE"
+  coin: "ticker"
 };
